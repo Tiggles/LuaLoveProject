74911dcb903100a536ab96e331e93dfc9e97ea36
Jonas Malte Hinchely <jonas@hinchely.dks>
2017-01-14 18:44:51 +0100

    Porper help

--- main.lua
+++ main.lua
@@ -164,11 +164,11 @@ function love.draw()
 	love.graphics.printf("Memory actually used (in kB): " .. memory_usage, 20, 80, 1000, "left")
 	love.graphics.printf("Entities drawn " .. entities_drawn, 20, 90, 1000, "left")
 	if keyboard_or_controller then
-		love.graphics.printf("Arrow keys for movement,", screen.width - 20, 90, 1000, "right")
+		love.graphics.printf("Arrow keys for movement, space to jump", screen.width - 255, 10, 1000, "left")
 	else
-		love.graphics.printf("Left button to add, right to remove,", screen.width - 250, 10, 1000, "left")
+		love.graphics.printf("Left button to add, right to remove,", screen.width - 255, 10, 1000, "left")
 	end
-	love.graphics.printf("'i' to swap state", screen.width - 250, 20, 1000, "left")
+	love.graphics.printf("'i' to swap state", screen.width - 255, 20, 1000, "left")
 	if last_memory_check + 1 < love.timer.getTime() then
 		memory_usage = collectgarbage("count")
 		last_memory_check = love.timer.getTime()
