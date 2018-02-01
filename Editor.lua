2ec9b536599b885bf3765832cdbe43d176730929
Jonas Malte Hinchely <jonas@hinchely.dk>
2018-02-01 16:45:58 +0100

    More functions for editor

--- Editor.lua
+++ Editor.lua
@@ -1,7 +1,16 @@
 Editor = {}
+local LUA_INDEX_OFFSET = 1
+local TYPE_COUNT = 4
+-- Types:
+-- -- 0. tiles
+-- -- 1. collectibles
+-- -- 2. events
+-- -- 3. actors
 
 function Editor:new()
     local editor = {
+        currentIndex = 0,
+        currentType = 0,
         tiles = {},
         collectibles = {},
         events = {},
@@ -25,4 +34,38 @@ end
 
 function Editor:addActorType(spritePath, scale_x, scale_y, width, height, is_friendly)
     table.insert(self.actors, ActorType:newType(spritePath, scale_x, scale_y, width, height, is_friendly))
+end
+
+function Editor:getCurrentTile()
+    if 0 == self.currentType - LUA_INDEX_OFFSET then
+        return self.tiles[self.currentIndex]
+    elseif 1 == self.currentType - LUA_INDEX_OFFSET then
+        return self.collectibles[self.currentIndex]
+    elseif 2 == self.currentType - LUA_INDEX_OFFSET then
+        return self.events[self.currentIndex]
+    elseif 3 == self.currentType - LUA_INDEX_OFFSET then
+        return self.actors[self.currentIndex]
+    end
+end
+
+function Editor:changeType(direction)
+    self.currentType = self.currentType + direction % TYPE_COUNT
+end
+
+function Editor:changeIndex(direction)
+    self.currentIndex = (self.currentIndex + direction) % self:getTypeCount()
+end
+
+function Editor:getTypeCount()
+    if 0 == self.currentType - LUA_INDEX_OFFSET then
+        return #self.tiles
+    elseif 1 == self.currentType - LUA_INDEX_OFFSET then
+        return #self.collectibles
+    elseif 2 == self.currentType - LUA_INDEX_OFFSET then
+        return #self.events
+    elseif 3 == self.currentType - LUA_INDEX_OFFSET then
+        return #self.actors
+    end
+    print("fault")
+    return "FAULT"
 end
